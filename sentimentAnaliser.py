3678cf2c2eda1799fb96e4c4dd902ce5dcc6b3e2
Saša Petalinkar <sasa5linkar@gmail.com>
2024-02-21 11:56:02 +0100

    first test. It seems to be working fine

--- sentimentAnaliser.py
+++ sentimentAnaliser.py
@@ -1,12 +1,40 @@
+import json
 import os
+import random
 from langchain.prompts import PromptTemplate
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 import sprwn_util
-from sprwn_util import get_def_by_words, srpwn
+from sprwn_util import get_def_by_words, srpwn, load_converted_synsets_from_file
+import joblib
+from tqdm import tqdm
+from langchain.prompts import PromptTemplate
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from tqdm import tqdm
+import logging
+import warnings
+import pandas as pd
+
+# Ignore warnings
+# Configure logging to write warnings to a log file
+logging.basicConfig(filename='warnings.log', level=logging.WARNING)
+
+# Redirect warnings to the logging system
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 def load_prompt_from_file(file):
-    with open(file, 'r') as f:
+    """Loads prompt from file.
+
+    Args:
+    file: str
+        File name.
+
+    Returns:
+    PromptTemplate: Prompt from file.
+
+    """
+    with open(file, 'r', encoding="utf-8") as f:
         return PromptTemplate.from_template(f.read())
 
 class SentimentAnaliser:
@@ -44,23 +72,63 @@ class SentimentAnaliser:
                                                      device=0)
         self.chain = self.prompt | self.lmm
     
-    def analise(self, text):
-        """Analises the text and sets the answer.
+    def analyze(self, text):
+        """Analyzes the text and sets the answer.
 
         Args:
-        text: str
-            Text to be analised.
+            text (str): Text to be analyzed.
+
+        Returns:
+            dict: The result of the analysis.
 
         """
+        return self.chain.invoke({"text": text})
 
-        self.asnwer = self.chain.invoke({"text": text})
+def analyse_list (list_text, sentimentAnaliser: SentimentAnaliser):
+    """Analises the list of texts and return list of tuples (text, answer).
 
+    Args:
+    list_text: list
+        List of texts to be analised.
+    sentimentAnaliser: SentimentAnaliser
+        SentimentAnaliser object used for analisys.
+    
+    Returns:
+    list: List of tuples (text, answer).
+
+    """
+    
+
+    return [(text, sentimentAnaliser.analyze(text)) for text in tqdm(list_text, desc="Analysing sentiments")]
+
+def save_sentment_list_as_cvs(list_text, sentimentAnaliser: SentimentAnaliser, file):
+    """Analises the list of texts and save it as cvs file, using ¦ as separator.
+
+    Args:
+    list_text: list
+        List of texts to be analised.
+    sentimentAnaliser: SentimentAnaliser
+        SentimentAnaliser object used for analisys.
+    file: str
+
+    """
+    list_result = analyse_list(list_text, sentimentAnaliser)
+    with open(file, 'w',encoding="utf-8") as f:
+        f.write("Text;Sentiment\n")
+        for text, result in list_result:
+            f.write(f"{text}¦{result}\n")
+    
+
+
+# Test prompt for sentiment analysis in English
 test_prompt = """
     As a sentiment analysis expert, analyze the following Serbian text and determine its sentiment. 
     The sentiment should be classified strictly as "positive", "negative", or "objective". No other responses will be accepted.
     Text: {text}
     Sentiment: 
 """
+# Test prompt for sentiment analysis in Serbian
+
 test_prompt_sr = """
     Kao ekspert za analizu sentimenta, analizirajte sledeći tekst na srpskom jeziku i odredite njegov sentiment. 
     Sentiment treba da bude striktno klasifikovan kao "pozitivan", "negativan", ili "objektivan". Nijedan drugi odgovor neće biti prihvaćen.
@@ -76,12 +144,19 @@ def main():
     text_list = list(filter(None, text_list))
 
     sa = SentimentAnaliser("mistralai/Mistral-7B-Instruct-v0.2", PromptTemplate.from_template(test_prompt_sr))
-    for text in text_list:
-        print(text)
-        print("++++++++++++++++++++++++++++++++++")
-        sa.analise(text)
-        print(sa.asnwer)
-        print("----------------------------------")
+    save_sentment_list_as_cvs(text_list, sa, "sentiment.csv")
+    print("Analised texts saved as sentiment.csv")
+    list_synsets = load_converted_synsets_from_file("converted_synsets")
+    #get random sample of 500 synsets from list
+    sample_synsets = random.sample(list_synsets, 500)
+    
+
+    for synset in tqdm(sample_synsets, desc="Processing synsets"):
+        synset["sentiment_sa"] = sa.analyze(synset["definition"])
+    with open("sample_synsets2.json", 'w',encoding="utf-8") as f:
+        f.write(json.dumps(sample_synsets, indent=4)) 
+    dataset = pd.DataFrame(sample_synsets)
+    dataset.to_csv("sample_synsets2.csv", sep="¦", index=False)
 
 if __name__ == "__main__":
     main()
